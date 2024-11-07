0c0a7e293d9b0503f342a6e777b602c658a8fd4c
jregithub <140324826+jregithub@users.noreply.github.com>
2024-11-07 11:57:35 -0500

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -2,9 +2,7 @@ def call(ip,warName,credID){
 sshagent(["${credID}"]) {
                     
 		    sh "scp -o StrictHostKeyChecking=no target/${warName}.war ec2-user@${ip}:/opt/tomcat9/webapps/"
-                    # stop tomcat
                    sh " ssh ec2-user@${ip} /opt/tomcat9/bin/shutdown.sh"
-                    # start tomcat
                     sh "ssh ec2-user@${ip} /opt/tomcat9/bin/startup.sh"
  }
 }
